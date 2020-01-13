eedcc97d7976c7ca8fc70a5b9bee26b1beb502b4
mozzan <linch0520@gmail.com>
2020-01-13 15:18:28 +0800

    not raise error when has duplicated keys

--- lib/ecpay_payment/core_ext/hash.rb
+++ lib/ecpay_payment/core_ext/hash.rb
@@ -5,9 +5,9 @@ class Hash
     each_key do |key|
       result[key.to_s] = self[key]
     end
-    unless result.keys.length() == orig_key_len
-        raise "Duplicate key name during convertion, there might be symbol & string key with the same name."
-    end
+    #unless result.keys.length() == orig_key_len
+    #    raise "Duplicate key name during convertion, there might be symbol & string key with the same name."
+    #end
     return result
   end
 end
